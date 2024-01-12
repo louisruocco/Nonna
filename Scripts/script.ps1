3c3f29b63a62e2085029762492b0ce9a9d61e76a
Louis Ruocco <louisruocco1@gmail.com>
2024-01-12 23:01:54 +0000

    added error logging to the Send-Email function

--- Scripts/script.ps1
+++ Scripts/script.ps1
@@ -1,13 +1,14 @@
+$date = Get-Date -Format "ddMMyyyy"
 # Check if each txt file exists
 $db = ".\db"
 $logs = ".\Logs"
 
-$remotePath = ".\db"
+$db = ".\db"
 
 $paths = @(
-    "$remotePath\gym.txt", 
-    "$remotePath\meal-planner.txt", 
-    "$remotePath\other learning.txt"
+    "$db\gym.txt", 
+    "$db\meal-planner.txt", 
+    "$db\other learning.txt"
 )
 
 Write-Host "Checking if db exists..."
@@ -36,8 +37,8 @@ if(!(Test-Path $logs)){
 # Call Brent Ozar Blog Web Scraper script
 # powershell.exe -File ".\scraper.ps1"
 
-$apiKey = Get-Content "$remotePath\secrets.txt"
-$topic = Get-Content "$remotePath\other learning.txt"
+$apiKey = Get-Content "$db\secrets.txt"
+$topic = Get-Content "$db\other learning.txt"
 $endpoint = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&q=$topic&key=$apiKey"
 $res = Invoke-RestMethod $endpoint
 $items = $res.items
@@ -70,13 +71,13 @@ function Pull-Data-From-DB {
 
 # Send email
 function Send-Email {
-    $username = (Get-Content "$remotePath\creds.txt")[0]
-    $password = (Get-Content "$remotePath\creds.txt")[1] | ConvertTo-SecureString -AsPlainText -Force
-    $emailAddress = (Get-Content "$remotePath\creds.txt")[2]
-    $gym = Get-Content "$remotePath\gym.txt"
-    $meals = Get-Content "$remotePath\meal-planner.txt"
-    $miscLearning = Get-Content "$remotePath\other learning.txt"
-    # $blogLink = Get-content "$remotePath\Brent Ozar Blog Links.txt" | Select-Object -Last 1
+    $username = (Get-Content "$db\creds.txt")[0]
+    $password = (Get-Content "$db\creds.txt")[1] | ConvertTo-SecureString -AsPlainText -Force
+    $emailAddress = (Get-Content "$db\creds.txt")[2]
+    $gym = Get-Content "$db\gym.txt"
+    $meals = Get-Content "$db\meal-planner.txt"
+    $miscLearning = Get-Content "$db\other learning.txt"
+    # $blogLink = Get-content "$db\Brent Ozar Blog Links.txt" | Select-Object -Last 1
 
     $mealPlanner = Randomise -db $meals
     $exercises = Randomise -db $gym
@@ -86,74 +87,95 @@ function Send-Email {
     $dinner = $mealplan[1]
 
     $day = (Get-Date).DayOfWeek
-    
-    if($day -eq "Tuesday" -or $day -eq "Thursday"){
-        $array = $exercises[0..7]
-        $gymExercises = foreach($exercise in $array){
-            "<li>$exercise</li>"
+    try {
+        if($day -eq "Tuesday" -or $day -eq "Thursday"){
+            $array = $exercises[0..7]
+            $gymExercises = foreach($exercise in $array){
+                "<li>$exercise</li>"
+            }
+        } else {
+            $gymExercises = "<h3>No Gym Today<h3>"
         }
-    } else {
-        $gymExercises = "<h3>No Gym Today<h3>"
-    }
 
-    if($day -eq "Monday"){
-        $learningLinks = foreach ($item in $learning){
-            "<li>$item</li>"
-        }
-        $body = @"
-        <h1>Nonna</h1>
-        <p>Hi Lou, Remember that I am always watching over you. Have a great day!. Love Nonna</p>
-        <h2>Learning Topic of the Week</h2>
-        <ul>
-            <li>$miscLearning</li>
-        </ul>
-        <h2>This Week's Learning Resources:</h2>
+        if($day -eq "Monday"){
+            $learningLinks = foreach ($item in $learning){
+                "<li>$item</li>"
+            }
+            $body = @"
+            <h1>Nonna</h1>
+            <p>Hi Lou, Remember that I am always watching over you. Have a great day!. Love Nonna</p>
+            <h2>Learning Topic of the Week</h2>
             <ul>
-                <li>$learningLinks</li>
+                <li>$miscLearning</li>
             </ul>
-        <h2>Today's Gym Session</h2>
-        <hr>
-        <ul>
-            $gymexercises
-        </ul>
-        <h2>Today's Meal Plan</h2>
-        <hr>
-        <h3>Lunch: $Lunch</h3>
-        <h3>Dinner: $dinner </h3>
+            <h2>This Week's Learning Resources:</h2>
+                <ul>
+                    <li>$learningLinks</li>
+                </ul>
+            <h2>Today's Gym Session</h2>
+            <hr>
+            <ul>
+                $gymexercises
+            </ul>
+            <h2>Today's Meal Plan</h2>
+            <hr>
+            <h3>Lunch: $Lunch</h3>
+            <h3>Dinner: $dinner </h3>
 "@
-    } else {
-        $body = @"
-        <h1>Nonna</h1>
-        <p>Hi Lou, Remember that I am always watching over you. Have a great day!. Love Nonna</p>
-        <h2>Learning Topic of the Week</h2>
-        <ul>
-            <li>$miscLearning</li>
-        </ul>
-        <h2>Today's Gym Session</h2>
-        <hr>
-        <ul>
-            $gymexercises
-        </ul>
-        <h2>Today's Meal Plan</h2>
-        <hr>
-        <h3>Lunch: $Lunch</h3>
-        <h3>Dinner: $dinner </h3>
+        } else {
+            $body = @"
+            <h1>Nonna</h1>
+            <p>Hi Lou, Remember that I am always watching over you. Have a great day!. Love Nonna</p>
+            <h2>Learning Topic of the Week</h2>
+            <ul>
+                <li>$miscLearning</li>
+            </ul>
+            <h2>Today's Gym Session</h2>
+            <hr>
+            <ul>
+                $gymexercises
+            </ul>
+            <h2>Today's Meal Plan</h2>
+            <hr>
+            <h3>Lunch: $Lunch</h3>
+            <h3>Dinner: $dinner </h3>
 "@
+            }
+
+        $email = @{
+            from = $username
+            to = $emailAddress
+            subject = "Nonna"
+            smtpserver = "smtp.gmail.com"
+            body = $body
+            port = 587
+            credential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
+            usessl = $true
+            verbose = $true
         }
 
-    $email = @{
-        from = $username
-        to = $emailAddress
-        subject = "Nonna"
-        smtpserver = "smtp.gmail.com"
-        body = $body
-        port = 587
-        credential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
-        usessl = $true
-        verbose = $true
-    }
+        Send-MailMessage @email -BodyAsHtml
+    } catch {
+        $Error | Out-File "$db\Logs\errorlog_$date.txt"
+        $body = @"
+        <h1>Nonna | Error Occurred</h1>
+        <p>Hi Lou, looks like an error occurred on my side. I'll log the error in $db\Logs\errorlog_$date.txt for you to review and fix. Sorry</p>
+"@
 
-    Send-MailMessage @email -BodyAsHtml
+        $email = @{
+            from = $username
+            to = $emailAddress
+            subject = "Nonna | Error Occured"
+            smtpserver = "smtp.gmail.com"
+            body = $body
+            port = 587
+            credential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
+            usessl = $true
+            verbose = $true
+        }
+
+        Send-MailMessage @email -BodyAsHtml
+    }
 }
 
 
